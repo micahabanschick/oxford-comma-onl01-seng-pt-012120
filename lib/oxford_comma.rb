43f8d7bdad8ac3a52ec1802ea93e98d52d53fb18
Micah Banschick <micha.ban@gmail.com>
2020-01-22 18:34:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ case array.length 
+  when 1 
+    array.join("")
+  when 2
+    array.join(" and ")
+  when 3
+    array[0..1].join(', ') + ", and " + array[2]
+  else 
+    array[0..array.length - 2].join(', ') + ", and " + array.last 
+  end 
 end
