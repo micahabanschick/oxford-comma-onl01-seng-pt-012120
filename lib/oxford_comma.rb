186f09b4b2fea0fa3f181b826fe91ee554602564
Micah Banschick <micha.ban@gmail.com>
2020-01-22 18:28:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   when 2
     array.join(" and ")
   when 3
-    array.join(", ")
+    array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
   else 
     array[0..-2].join(', ') + " and " + array[-1] 
   end 
